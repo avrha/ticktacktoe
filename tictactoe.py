8f82e6f0571bb51aa8c9e9684f141b60685e3910
Joey Ferenchak <sjferenchak@gmail.com>
2021-03-27 11:23:18 -0400

    able to print out board

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 class Tictactoe:
   def __init__(self):
     self.coords = {
@@ -17,7 +18,9 @@ class Tictactoe:
     } 
 
   def print_board(self):
-    print(self.coords["1,1"])
+    print(self.coords["1,1"]+"|"+self.coords["1,2"]+"|"+self.coords["1,3"])
+    print(self.coords["2,1"]+"|"+self.coords["2,2"]+"|"+self.coords["2,3"])
+    print(self.coords["3,1"]+"|"+self.coords["3,2"]+"|"+self.coords["3,3"])
 
 def main():
   obj = Tictactoe()
